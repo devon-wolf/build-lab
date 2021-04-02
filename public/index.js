145fd5e9eb215ca48a18bf394d70d1cc06f0ec7a
Devon Wolfkiel <devonwolf@tutanota.com>
2021-04-02 14:50:30 -0700

    Get frontend wired up to backend post endpoint

--- public/index.js
+++ public/index.js
@@ -2,23 +2,58 @@ const searchForm = document.querySelector('form');
 const searchInput = document.querySelector('input');
 const resultsSection = document.getElementById('search-results');
 const wordHeader = document.getElementById('user-word');
-const synPar = document.getElementById('syns');
-const antPar = document.getElementById('ants');
-const defPar = document.getElementById('def');
-const saveButton = document.getElementById('save-button');
+const synsEl = document.getElementById('syns');
+const antsEl = document.getElementById('ants');
+const defEl = document.getElementById('def');
+
+const displayResults = (results, parentEl) => {
+	if (!results) {
+		const none = document.createElement('li');
+		none.textContent = 'no results';
+		parentEl.append(none);
+	}
+	else {
+		for (let item of results) {
+			const li = document.createElement('li');
+			li.textContent = item;
+			parentEl.append(li);
+		}
+	}
+}
 
 searchForm.addEventListener('submit', e => {
 	e.preventDefault();
-	const formData = new FormData(searchForm);
-	const searchQuery = formData.get('search-query');
-	resultsSection.classList.remove('none');
-	wordHeader.textContent = searchQuery;
-	synPar.textContent = 'synonyms';
-	antPar.textContent = 'antonyms';
-	defPar.textContent = 'definition';
-	searchInput.value = '';
-});
 
-saveButton.addEventListener('click', () => {
-	console.log('.... you know nothing got saved, right?');
+	const allLis = Array.from(document.querySelectorAll('li'));
+	console.log(allLis);
+	if (allLis.length) {
+		for (let li of allLis) {
+			li.remove();
+		}
+	}
+
+	const formData = new FormData(searchForm);
+	
+	fetch('/api/v1/words', {
+		method: 'POST',
+		headers: {
+		  'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+		  word: formData.get('search-query'),
+		}),
+	  })
+	  .then(res => res.json())
+	  .then(
+		  ({ word, synonyms, antonyms, definition }) =>  {
+			wordHeader.textContent = word;
+			displayResults(synonyms, synsEl);
+			displayResults(antonyms, antsEl);
+			displayResults(definition, defEl);
+		  }
+	  );
+
+	searchInput.value = '';
+	resultsSection.classList.remove('none');
+	
 });
