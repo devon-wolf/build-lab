054be35c7589c03f6888b47d41abbb33849eb8dd
Devon Wolfkiel <devonwolf@tutanota.com>
2021-04-01 17:45:09 -0700

    Add initial form submit

--- public/index.js
+++ public/index.js
@@ -5,3 +5,11 @@ const synPar = document.getElementById('syns');
 const antPar = document.getElementById('ants');
 const defPar = document.getElementById('def');
 const saveButton = document.getElementById('save-button');
+
+searchForm.addEventListener('submit', e => {
+	e.preventDefault();
+	const formData = new FormData(searchForm);
+	const searchQuery = formData.get('search-query');
+	resultsSection.classList.remove('none');
+	wordHeader.textContent = searchQuery;
+})
