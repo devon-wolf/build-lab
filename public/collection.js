81b2ae4c268e6666d912798702b9a0e5fff43f0e
Devon Wolfkiel <devonwolf@tutanota.com>
2021-04-02 15:14:10 -0700

    Add delete endpoint functionality to wordlist

--- public/collection.js
+++ public/collection.js
@@ -10,9 +10,24 @@ fetch('/api/v1/words', {
   .then(
 	  res => {
 		for (let word of res) {
-			const testEl = document.createElement('li');
-			testEl.textContent = word.word;
-			wordList.append(testEl);
+			const entry = document.createElement('li');
+			const wordResult = document.createElement('h3');
+			wordResult.textContent = word.word;
+
+			const removeButton = document.createElement('button');
+			removeButton.textContent = 'X';
+			removeButton.addEventListener('click', () => {
+				fetch(`/api/v1/words/${word.id}`, {
+					method: 'DELETE',
+					headers: {
+						'Content-Type': 'application/json',
+					}
+				})
+				.then(entry.remove());
+			});
+			entry.append(wordResult);
+			entry.append(removeButton);
+			wordList.append(entry);
 		}
 	  }
   );
