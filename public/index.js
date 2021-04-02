c488229acd579d3ab71084270346efac93823a63
Devon Wolfkiel <devonwolf@tutanota.com>
2021-04-01 17:33:56 -0700

    Add initial HTML and JS for the frontend

---
+++ public/index.js
@@ -0,0 +1,7 @@
+const searchForm = document.querySelector('form');
+const resultsSection = document.getElementById('search-results');
+const wordHeader = document.getElementById('user-word');
+const synPar = document.getElementById('syns');
+const antPar = document.getElementById('ants');
+const defPar = document.getElementById('def');
+const saveButton = document.getElementById('save-button');
